9fd71e2229b6a1fb4be456dc76481be06f7cd434
Jesus Gonzalez Memije <jesus.gonzalezm@elektra.com.mx>
2024-07-20 12:07:18 -0600

    Test

--- app/src/main/java/com/memije/movienao/MainActivity.kt
+++ app/src/main/java/com/memije/movienao/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieNaoTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",

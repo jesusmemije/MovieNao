d9df4c9753d11343ef2618c0d6621a236a59deae
Jesus Gonzalez Memije <jesus.gonzalezm@elektra.com.mx>
2024-07-20 23:48:00 -0600

    Carpeta Utils con fuctions custom color TexFields

--- app/src/main/java/com/memije/movienao/ui/LoginScreen.kt
+++ app/src/main/java/com/memije/movienao/ui/LoginScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -44,6 +42,7 @@ import com.memije.movienao.ui.theme.BlueApp
 import com.memije.movienao.ui.theme.GreenApp
 import com.memije.movienao.ui.theme.MovieNaoTheme
 import com.memije.movienao.ui.theme.WhiteApp
+import com.memije.movienao.ui.utils.getCustomTextFieldColors
 
 @Composable
 fun LoginScreen() {
@@ -102,21 +101,7 @@ fun Password() {
         onValueChange = { },
         modifier = Modifier.fillMaxWidth(),
         label = { Text("Password") },
-        colors = OutlinedTextFieldDefaults.colors(
-            unfocusedBorderColor = WhiteApp,
-            unfocusedPlaceholderColor = WhiteApp,
-            unfocusedContainerColor = Color.Transparent,
-            unfocusedLeadingIconColor = GreenApp,
-            unfocusedTrailingIconColor = WhiteApp,
-            unfocusedLabelColor = WhiteApp,
-
-            focusedBorderColor = GreenApp,
-            focusedPlaceholderColor = GreenApp,
-            focusedContainerColor = Color.Transparent,
-            focusedLeadingIconColor = GreenApp,
-            focusedTrailingIconColor = GreenApp,
-            focusedLabelColor = GreenApp
-        ),
+        colors = getCustomTextFieldColors(),
         singleLine = true,
         maxLines = 1,
         leadingIcon = {
@@ -158,24 +143,9 @@ fun Email() {
             )
         },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        colors = OutlinedTextFieldDefaults.colors(
-            unfocusedBorderColor = WhiteApp,
-            unfocusedPlaceholderColor = WhiteApp,
-            unfocusedContainerColor = Color.Transparent,
-            unfocusedLeadingIconColor = GreenApp,
-            unfocusedTrailingIconColor = WhiteApp,
-            unfocusedLabelColor = WhiteApp,
-
-            focusedBorderColor = GreenApp,
-            focusedPlaceholderColor = GreenApp,
-            focusedContainerColor = Color.Transparent,
-            focusedLeadingIconColor = GreenApp,
-            focusedTrailingIconColor = GreenApp,
-            focusedLabelColor = GreenApp
-        )
+        colors = getCustomTextFieldColors()
     )
 }
-
 
 @Preview(showBackground = true)
 @Composable

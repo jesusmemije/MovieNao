b3216e6f79ba3c6a1d6f09eb6e5c3591882752d3
Jesus Gonzalez Memije <jesus.gonzalezm@elektra.com.mx>
2024-07-21 16:26:35 -0600

    Shadow en TopBar

--- app/src/main/java/com/memije/movienao/ui/utils/TopAppBarCustom.kt
+++ app/src/main/java/com/memije/movienao/ui/utils/TopAppBarCustom.kt
@@ -6,7 +6,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -24,6 +28,10 @@ fun TopAppBarCustom(navController: NavHostController) {
         }, colors = TopAppBarDefaults.topAppBarColors(
             containerColor = BlackApp,
             titleContentColor = WhiteApp,
+        ),
+        modifier = Modifier.shadow(
+            elevation = 10.dp, //or whatever value
+            spotColor = Color.Black
         )
     )
 }
